172e08e809d0b67b3a226fe551a121c9b3e153f2
vks-jrobertson <jrobertson@vksapp.com>
2020-11-05 22:27:14 +0000

    DEV-pre, promiseTest updated with variables

--- promiseTest.js
+++ promiseTest.js
@@ -1,7 +1,12 @@
 const { getGoogleData } = require('./lib/getGoogleData.js')
 
 async function getStuff() {
-  const alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", 'z', 0,1,2,3,4,5,6,7,8,9]
+  const alphaAfter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", 'z']
+  const questionsBefore = ['when', 'will', 'what', 'how', 'where','why','are','who', 'which', 'can']
+  const prepositionsAfter = ['can', 'for', 'to', 'near', 'is', 'without', 'with']
+  const comparisonsAfter = ['like', 'or', 'versus', 'vs', 'and']
+  const priceBefore = ['cheapest', 'cheap', 'best']
+  const topBefore = ['top 10', 'top 5', 'top 3', 'top']
 
   let results = []
   for(const item of alpha) {
