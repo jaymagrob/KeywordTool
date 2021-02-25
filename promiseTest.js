8c2578072d1dbfb93c9e9daeee3dd93e6b3b934e
vks-jrobertson <jrobertson@vksapp.com>
2021-02-25 14:13:44 +0000

    DEV-pre: added airbnb linter and changes

--- promiseTest.js
+++ promiseTest.js
@@ -1,20 +1,20 @@
-const { getGoogleData } = require('./lib/getGoogleData.js')
+const { getGoogleData } = require('./lib/getGoogleData.js');
 
 async function getStuff() {
-  const alphaAfter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", 'z']
-  const questionsBefore = ['when', 'will', 'what', 'how', 'where','why','are','who', 'which', 'can']
-  const prepositionsAfter = ['can', 'for', 'to', 'near', 'is', 'without', 'with']
-  const comparisonsAfter = ['like', 'or', 'versus', 'vs', 'and']
-  const priceBefore = ['cheapest', 'cheap', 'best']
-  const topBefore = ['top 10', 'top 5', 'top 3', 'top']
-  const one = ['skateboard']
+  const alphaAfter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const questionsBefore = ['when', 'will', 'what', 'how', 'where', 'why', 'are', 'who', 'which', 'can'];
+  const prepositionsAfter = ['can', 'for', 'to', 'near', 'is', 'without', 'with'];
+  const comparisonsAfter = ['like', 'or', 'versus', 'vs', 'and'];
+  const priceBefore = ['cheapest', 'cheap', 'best'];
+  const topBefore = ['top 10', 'top 5', 'top 3', 'top'];
+  const one = ['skateboard']; // first testing
 
-  let results = {}
-  for(const item of one) {
-    const r = await getGoogleData(item)
+  const results = {};
+  for (const item of one) {
+    const r = await getGoogleData(item);
     results[item] = r[0];
   }
-  return results
+  return results;
 }
 
-getStuff()
+getStuff();
