54b0e9a6dc60b9ce9e234b0eeb6334bebcfab2e9
vks-jrobertson <johnmagnusrobertson@gmail.com>
2021-07-05 21:22:09 +0100

    DEV-pre: updated jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable max-len */
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  coverageDirectory: 'coverage',
+  testURL: 'http://localhost',
+};
