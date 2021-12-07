81ae0c9b07e32a0eec0cedffdb56087e83dc1512
John Robertson <johnmagnusrobertson@gmail.com>
2021-12-07 23:21:51 +0000

    DEV-11, Search: controller updated

--- controllers/search.js
+++ controllers/search.js
@@ -1,14 +1,12 @@
-const { getGoogleData } = require('../lib/getGoogleData');
-const { formatGoogleData } = require('../lib/formatGoogleData');
-
-const alphaAfter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const GetGoogle = require('../lib/Google');
+const Word = require('../lib/Words');
 
 function find(req, res) {
   const {
-    country, sdfsdfm, query, checks,
+    country, query, checks,
   } = req.body;
 
-  Promise.all(alphaAfter.map((i) => getGoogleData(`${query} ${i}`, checks)))
+  Word.getWords(query, new GetGoogle(query).getData)
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json(err.message));
 }
