3c029899b5bc5f3555a2618895882f59492b486d
John Robertson <johnmagnusrobertson@gmail.com>
2022-01-27 18:40:32 +0000

    DEV-22, index: add process.env.PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./config/router');
 
 const app = express();
-const port = 3456;
+const port = process.env.PORT || 3456;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
