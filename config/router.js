d4e77951774bf98da585e5c277032acb0bb173f0
John Robertson <johnmagnusrobertson@gmail.com>
2022-02-01 21:19:24 +0000

    DEV-32, router: country added

--- config/router.js
+++ config/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const search = require('../controllers/search');
 
-router.route('/search/:query/')
+router.route('/search/:query/:country/')
   .get(search.find);
 
 module.exports = router;
