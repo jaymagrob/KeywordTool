3acbbdb708e249367da05f603b4f5e42cff5b35a
John Robertson <johnmagnusrobertson@gmail.com>
2022-02-08 20:59:26 +0000

    DEV-35, search controller: add language

--- be/controllers/search.ts
+++ be/controllers/search.ts
@@ -1,10 +1,10 @@
 import GetGoogle from "../lib/Google";
 import Word from "../lib/Words";
 
-export function find(req, res) {
-  const { country, query, checks } = req.params;
+export default function find(req, res) {
+  const { country, query, language } = req.params;
 
-  Word.getWords(query, new GetGoogle(query, country).getData)
+  Word.getWords(query, new GetGoogle(query, country, language).getData)
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json(err.message));
 }
